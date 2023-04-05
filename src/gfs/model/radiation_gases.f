3e9407e540f87f2cacd21269db0ee9686fe36b75
rashmi <rashmikakatkar@gmail.com>
2023-04-05 10:26:15 +0530

    Path for GHG INPUT data

--- src/gfs/model/radiation_gases.f
+++ src/gfs/model/radiation_gases.f
@@ -10,7 +10,7 @@
 !                                                                      !
 !      'gasinit'    -- initialization                                  !
 !         input:                                                       !
-!           ( iyear, month, ICTM, ICO2, me )                           !
+!           ( iyear, month, ICTM, ICO2, me, MNYEAR )                   !
 !         output:                                                      !
 !           ( none )                                                   !
 !                                                                      !
@@ -96,7 +96,7 @@
       integer, parameter         :: IMXCO2  = 24     ! input co2 data lon points
       integer, parameter         :: JMXCO2  = 12     ! input co2 data lat points
 !!!!! edit by Dr. Rashmi Kakatkar : MINYEAR is not fixed in this updated
-!code. But this will be provided in gfs_namelist as mnyear.
+!code. This will be provided in gfs_namelist as mnyear.
       integer         :: MINYEAR = 1950   
 
       real (kind=kind_phys), parameter :: resco2=15.0         ! horiz res in degree
@@ -218,7 +218,9 @@
       implicit none
 
 !  ---  inputs:
+
 !!!!!edit by Dr. Rashmi Kakatkar  : MNYEAR is added in input
+
       integer, intent(in) :: iyear, month, ICTM, ICO2, me, MNYEAR
 
 !  ---  output: ( none )
@@ -229,33 +231,38 @@
       real (kind=kind_phys):: n2og1,ch4g1,f11g1,f12g1,f22g1,cl4g1,f113g1
       integer    :: i, iyr, imo, iyr1, iyr2, jyr, idyr
       logical    :: file_exist, lextpl
-!      character  :: cline*100, cform*8, cfile0*26, cfile1*26,           &
-!!!!!edit by Dr. Rashmi Kakatkar   cfile0 and cfile1 paths are changed
+
+!!!!!edit by Dr. Rashmi Kakatkar :  cfile0 and cfile1 paths are changed
 !hence accordingly this number
-      character  :: cline*100, cform*8, cfile0*34, cfile1*34,           &
+
+!      character  :: cline*100, cform*8, cfile0*26, cfile1*26,           &
+      character  :: cline*100, cform*8, cfile0*44, cfile1*44,           &
      &              cfuser*26, cfmcyc*26
 
       data  cfuser / 'co2userdata.txt           ' /
       data  cfmcyc / 'co2monthlycyc.txt         ' /
 !      data  cfile0 / 'co2historicaldata_glob.txt' /
 !      data  cfile1 / 'co2historicaldata_2004.txt' /
-!!!!!edit by Dr. Rashmi Kakatkar : ghg data is inside gfs_fix folder and
-!filenames are also changed 
-      data  cfile0 / 'ghg_fix/ghgforcingdecadal_glob.txt' /
-      data  cfile1 / 'ghg_fix/ghgforcingdecadal_2004.txt' /
+
+!!!!!edit by Dr. Rashmi Kakatkar : ghg data is inside INPUT/ghg_forcing folder
+! and filenames are also changed 
+
+      data  cfile0 / 'INPUT/ghg_forcing/ghgforcingdecadal_glob.txt' /
+      data  cfile1 / 'INPIT/ghg_forcing/ghgforcingdecadal_2004.txt' /
       data  cform  / '(24f7.2)' /       !! data format in IMXCO2*f7.2
 
 !===>  ...  begin here
 
 !!!!!edit by Dr. Rashmi Kakatkar   : MNYEAR from gfs_namelist is
 !provided here to MINYEAR instead of fixing it initially in this code 
+
       MINYEAR = MNYEAR
       ico2flg = ICO2
 
 !!!!!edit by Dr. Rashmi Kakatkar : Print to check whether mnyear is
 !correctly read
        if ( me == 0 ) then
-          print *,' MINYEAR=',   MINYEAR
+          print *,'check : MINYEAR=',   MINYEAR
         endif
 
       if ( ICO2 == 0 ) then
@@ -390,7 +397,7 @@
 !  --- ...  set up input data file name
           if ( ICTM == -2 ) then           ! add seasonal cycle to data at ic time
             cfile1 = cfile0
-            write(cfile1(19:22),34) idyr
+            write(cfile1(37:40),34) idyr
   34        format(i4.4)
 
             if ( me == 0 ) then
@@ -405,10 +412,10 @@
             endif
           else                             ! use historical observed data
             cfile1 = cfile0
-!            write(cfile1(19:22),34) idyr
+!            write(cfile1(37:40),34) idyr
 
 !!!!!edit by Dr. Rashmi Kakatkar  : as per new path of ghg data
-            write(cfile1(27:30),34) idyr
+            write(cfile1(37:40),34) idyr
 
             if ( me == 0 ) then
               print *,' - Using Historical Co2 Data Table'
@@ -449,7 +456,7 @@
 !                write(cfile1(19:22),34) iyr
 
 !!!!!edit by Dr. Rashmi Kakatkar : as per new path of ghg data
-                write(cfile1(27:30),34) iyr
+                write(cfile1(37:40),34) iyr
 
                 inquire (file=cfile1, exist=file_exist)
                 if ( me == 0 ) then
